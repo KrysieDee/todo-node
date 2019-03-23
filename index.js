9398a156357c3e064e39bc33f8700a6b31e8e2bf
KrysieDee <krystal.smithx@gmail.com>
2019-03-23 13:31:01 -0400

    Parser add

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+
 
 app.set('view engine', 'ejs'); 
 app.get('/', function (req, res) {
@@ -8,6 +10,7 @@ res.render('index');
 app.post('/addtask', function (req, res){
 res.render('index')
 }); 
+app.use(bodyParser.urlencoded({  extended: true })); 
 app.listen(3000, function (){
 console.log('Example app listening of port 3000!')
 }); 
