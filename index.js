b16b0fa1f80123a40bd1023bcab9bdbb571dd27d
KrysieDee <krystal.smithx@gmail.com>
2019-03-23 13:26:20 -0400

    Post request

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
 res.render('index'); 
 }); 
-
+app.post('/addtask', function (req, res){
+res.render('index')
+}); 
 app.listen(3000, function (){
 console.log('Example app listening of port 3000!')
 }); 
